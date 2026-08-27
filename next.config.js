7c56bfe4d5a3ee6beb0473371258ddbf12e0799c
sandeepGM75 <sandeep.glidedmatrix@gmail.com>
2026-08-28 01:05:51 +0530

    build fixeds

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com', 'plus.unsplash.com'],
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'plus.unsplash.com' },
+    ],
     unoptimized: true,
   },
 }
